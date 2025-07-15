2402f4b5bb46268b1b2f5a414d2994a3c358aa90
imago <l.kinde94@outlook.com>
2025-07-15 21:23:25 +0300

    refactor: Made cmd package reusable for more projects
    Also includes a lot of cleaning and QoL

--- cmd/serve/handlers.go
+++ cmd/serve/handlers.go
@@ -6,21 +6,21 @@ import (
 	"github.com/baalimago/go_away_boilerplate/pkg/ancli"
 )
 
-func slogHandler(next http.Handler) http.Handler {
+func SlogHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ancli.PrintfOK("%s - %s", r.Method, r.URL.Path)
+		ancli.Okf("%s - %s", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
 }
 
-func cacheHandler(next http.Handler, cacheControl string) http.Handler {
+func CacheHandler(next http.Handler, cacheControl string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Cache-Control", cacheControl)
 		next.ServeHTTP(w, r)
 	})
 }
 
-func crossOriginIsolationHandler(next http.Handler) http.Handler {
+func CrossOriginIsolationHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Cross-Origin-Opener-Policy", "same-origin")
 		w.Header().Add("Cross-Origin-Embedder-Policy", "require-corp")
